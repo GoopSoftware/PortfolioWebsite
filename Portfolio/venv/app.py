64ce42b01a50c1cee196fc34819054b1f696a449
GoopSoftware <jhreschke@gmail.com>
2025-02-09 23:12:15 -0700

    2/9 Commit: Created basic structure of website, macros for projects page

--- Portfolio/venv/app.py
+++ Portfolio/venv/app.py
@@ -10,6 +10,24 @@ def home():
 def projects():
     return render_template('projects.html')
 
+@app.route('/projects/adman')
+def adman():
+    return render_template(
+    'macros.html',
+    video_filename = 'videos/A-D man.mp4',
+    title = 'A-D Man',
+    description = ('A-D Man was my opportunity to challenge myself and put into practice '
+                   'everything I had learned up until that point. The idea behind the game '
+                   'is souls-like, two button controls, and pursuit. The game features a '
+                   ' game speed mechanic that increases or decreases according to player '
+                   'ability. The game was built using an object-oriented design structure.'),
+    github_url = 'https://github.com/GoopSoftware/ADMan',
+    skills = ["Object-Oriented Programming", "Pointers/References", "Vectors",
+              "Adaptive Gameplay"]
+    )
+
+
+
 @app.route('/about')
 def about():
     return render_template('about.html')
